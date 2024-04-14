ddfdfdef79de8bebec3ddc9679320f3cbc4bcbb0
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-04-14 17:31:02 -0300

    third commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,18 @@
+import img from '../../assets/images/invictus_go_Almaty_gym_zone_2022.jpeg'
+
 export default function Card() {
 
     return (
         <section className="bg-gray-600 rounded-full p-5 m-5" >
-            <div className="" >
+            <div>
                 <h1 className="text-3xl m-5" >Teste</h1>
 
                 <p className="text-xl" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga.</p>
+
+                <div className="flex justify-center items-center mt-5" >
+                    <img src={img} alt="" className='h-1/2 w-1/2' />
+                </div>
+
             </div>
         </section>
     )
