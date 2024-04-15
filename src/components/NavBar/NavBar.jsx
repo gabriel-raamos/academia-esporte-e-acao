6e5b189f854eea3f92232d38f70c58004f210967
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-04-15 14:28:39 -0300

    eigth commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,16 @@
 import NavButton from "../NavButton/NavButton";
-import LogoNoText from "../LogoNoText/LogoNoText";
+// import LogoNoText from "../LogoNoText/LogoNoText";
+import logo from '../../assets/2-removebg-preview.svg'
 
 export default function NavBar() {
     return (
 
-        <nav className="grid grid-cols-4" >
+        <nav className="grid grid-cols-3" >
 
             <div className="flex items-center justify-center text-red-700 hover:bg-red-700 hover:text-white transition duration-500 cursor-pointer" >
-                {/* <img src={logo}/> */}
-                <LogoNoText />
+                <img src={logo} style={{ color: 'red' }} className="w-1/3 h-1/3" />
+                {/* <LogoNoText /> */}
             </div>
-
-            <NavButton title="Home" />
 
             <NavButton title="Planos" />
 
--- src/components/NavButton/NavButton.jsx
+++ src/components/NavButton/NavButton.jsx
@@ -2,9 +2,14 @@ import PropTypes from 'prop-types'
 
 function NavButton(props) {
     return (
-        <button className="text-red-700 font-bold transition duration-500 hover:bg-red-700 hover:text-white text-xl" >
-            {props.title}
-        </button>
+
+        <div className="flex justify-center items-center" >
+            <button className="text-red-700 p-7 px-10 rounded-3xl font-bold transition duration-500 hover:bg-red-700 hover:text-white text-xl" >
+                {props.title}
+            </button>
+
+        </div>
+
     )
 }
 
