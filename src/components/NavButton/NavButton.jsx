200fed5fed36707f9d550980ea40e0de7a79d5a5
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-04 00:53:36 -0300

    test navbutton

--- src/components/NavButton/NavButton.jsx
+++ src/components/NavButton/NavButton.jsx
@@ -6,7 +6,7 @@ function NavButton({link, title}) {
 
         <div className="flex justify-center items-center h-2/3 w-2/3" >
             <Link to={`${link}`}>
-                <button className="text-red-700 text-sm md:text-xl py-2 md:py-5 px-5 mt-7 md:mt-0 rounded-full font-bold transition duration-500 hover:bg-red-700 hover:text-white" >
+                <button className="text-red-700 text-sm md:text-xl py-2 md:py-5 px-2 mt-7 md:mt-0 rounded-full font-bold transition duration-500 hover:bg-red-700 hover:text-white" >
                     {title}
                 </button>
             </Link>
