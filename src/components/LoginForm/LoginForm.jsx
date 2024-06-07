fc6e546e615e71ba1f187d7091bfc35525673585
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-07 18:51:45 -0300

    page is now autoreloading after login, but still crashing

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -24,7 +24,7 @@ export default function LoginForm() {
 
             alert('Login efetuado com sucesso.');
 
-            // window.location.reload()
+            window.location.reload()
         } catch (error) {
             alert("Error: " + error.message);
         }
