fcfeb285a371d9bfbb1eb186890ee63fbc1718f3
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-09 17:57:06 -0300

    trying to correct login problem

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -16,7 +16,7 @@ export default function LoginForm() {
         };
 
         try {
-            const response = await axios.post("http://localhost:5000/cliente/logarcliente", userData);
+            const response = await axios.post("https://pi-academia.vercel.app/cliente/logarcliente", userData);
 
             localStorage.setItem('authorization', response.data.accessToken)
             localStorage.setItem('json-data', JSON.stringify(response.data.clienteData))
