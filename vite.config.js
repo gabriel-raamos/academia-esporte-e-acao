d39a024cb07e7c9304a73022a583a9a83e33aa87
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-10 01:15:01 -0300

    trying again to solve 404 error

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   },
+  server: {
+    port: 3000
+  }
 });
 
 // https://vitejs.dev/config/
