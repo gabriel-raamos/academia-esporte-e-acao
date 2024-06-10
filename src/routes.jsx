8d81048561d28f3fe9d92de05e8cf36602a2f345
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-10 01:19:43 -0300

    again

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,32 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar'
-// import PaginaPadrao from './components/PaginaPadrao/PaginaPadrao'
-import Home from './pages/Home/Home'
-import Planos from './pages/Planos/Planos'
-import Login from './pages/Login/Login'
-import Rodape from './components/Rodape/Rodape'
-import FaleConosco from './pages/FaleConosco/FaleConosco'
-import Dashboard from './pages/Dashboard/Dashboard'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import Planos from './pages/Planos/Planos';
+import Login from './pages/Login/Login';
+import Rodape from './components/Rodape/Rodape';
+import FaleConosco from './pages/FaleConosco/FaleConosco';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 export default function App() {
   return (
     <BrowserRouter>
       <NavBar />
-
-      <div className='app-container' >
+      <div className='app-container'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/planos" element={<Planos />} />
           <Route path="/faleconosco" element={<FaleConosco />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
-
-      <div className='footer' >
+      <div className='footer'>
         <Rodape />
       </div>
-
     </BrowserRouter>
-  )
+  );
 }
