06f1ae5f0c133be35b68b0269d6427af2e5fc927
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-12 00:12:02 -0300

    added ternary to usuarios page

--- src/pages/Usuarios/Usuarios.jsx
+++ src/pages/Usuarios/Usuarios.jsx
@@ -1,10 +1,30 @@
+import { Link } from "react-router-dom"
+
 export default function Usuarios() {
 
-    const role = JSON.parse(localStorage.getItem('json-data')).name
+    const role = JSON.parse(localStorage.getItem('json-data')).role
 
     return (
-        <section className="flex justify-center items-center" >
-            <p>{role}</p>
+        <section>
+            {role == 'admin' ? (
+                <div className="flex justify-center items-center" >
+                    <p className="bg-red-700 text-white rounded-full font-bold p-5 my-5 text-xl" >{role}</p>
+                </div>
+            ) : (
+                <div className="justify-center items-center" >
+                    <div className="flex justify-center items-center" >
+                        <p className="bg-red-700 text-white rounded-full font-bold p-5 my-5 text-xl" >Você não deveria estar aqui.</p>
+                    </div>
+
+                    <div className="flex justify-center items-center" >
+                        <Link to='../' >
+                            <button className="bg-red-700 text-white rounded-full font-bold p-5 my-5 text-xl" >
+                                Clique aqui para voltar para a home page.
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            )}
         </section>
     )
 }
