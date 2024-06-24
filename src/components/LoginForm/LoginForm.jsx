5af294a286aaec4f1fd177bc61dcc64b751dc3d7
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-24 00:06:52 -0300

    changed from localhost to pi-academia vercel

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -16,7 +16,7 @@ export default function LoginForm() {
         };
 
         try {
-            const response = await axios.post("http://localhost:5000/api/cliente/logarcliente", userData, {
+            const response = await axios.post("https://pi-academia.vercel.app/api/cliente/logarcliente", userData, {
                 withCredentials: true
             });
 
