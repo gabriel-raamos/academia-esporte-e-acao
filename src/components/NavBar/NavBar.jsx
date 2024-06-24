7faedc01578c653f1c869c875f9d8e6cc154e4ed
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-24 14:02:42 -0300

    just added logout requisition to logout method in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -36,7 +36,7 @@ export default function NavBar() {
     const logout = async () => {
 
         try {
-            // await axios.post('http://localhost:5000/api/cliente/logout')
+            await axios.post('http://localhost:5000/api/cliente/logout')
 
             localStorage.removeItem('authorization')
             localStorage.removeItem('json-data')
