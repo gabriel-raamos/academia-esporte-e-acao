4094e9bc51932e97fec22b557b0fd60c649dfc44
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-06-27 03:25:56 -0300

    changed Treinos page

--- src/pages/Treinos/Treinos.jsx
+++ src/pages/Treinos/Treinos.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+
 export default function Treinos() {
     const [workouts, setWorkouts] = useState([]);
     const [currentWorkoutIndex, setCurrentWorkoutIndex] = useState(0);
@@ -98,11 +101,11 @@ export default function Treinos() {
             {active ? (
                 <div>
                     {role === 'admin' && (
-                        <div className="grid grid-rows-2 justify-center items-center">
+                        <div className="flex flex-col justify-center items-center">
                             <div className="flex justify-center">
                                 <Link to="../usuarios">
                                     <button
-                                        className="bg-blue-700 text-white rounded-full font-bold p-5 my-5 text-xl transition hover:bg-blue-500 hover:-translate-y-1 duration-500"
+                                        className="bg-blue-700 text-white rounded-full font-bold p-5 my-5 text-xl transition hover:scale-110 active:bg-blue-900 hover:-translate-y-1 duration-500"
                                     >
                                         Ir para Usuários
                                     </button>
@@ -112,7 +115,7 @@ export default function Treinos() {
                             <div className="flex justify-center">
                                 <Link to="../pagamentolista">
                                     <button
-                                        className="bg-blue-700 text-white rounded-full font-bold p-5 my-5 text-xl transition hover:bg-blue-500 hover:-translate-y-1 duration-500"
+                                        className="bg-blue-700 text-white rounded-full font-bold p-5 my-5 text-xl transition hover:scale-110 active:bg-blue-900 hover:-translate-y-1 duration-500"
                                     >
                                         Ir para histórico de pagamentos
                                     </button>
@@ -123,37 +126,55 @@ export default function Treinos() {
                     {workouts.length > 0 ? (
                         <section className="mb-5">
                             <div className="flex justify-center items-center">
-                                <h2 className="bg-blue-700 text-white rounded-full font-bold p-5 my-5 text-xl">
-                                    Informação sobre os treinos
+                                <h2 className="bg-blue-700 text-white rounded-lg font-bold p-5 my-5 text-xl select-none">
+                                    Informação sobre os treinos:
                                 </h2>
                             </div>
 
-                            <div className="flex justify-center">
-                                <div className="border-4 border-blue-700 rounded-xl">
-                                    <div className="flex justify-center text-blue-700 font-bold my-5 text-lg">
-                                        <p>Treino {currentWorkoutIndex + 1} de {workouts.length}</p>
+                            <div className='flex justify-center' >
+                                <div className="flex flex-col justify-center md:w-1/2">
+
+                                    <div className="flex justify-center items-center mt-4">
+                                        {/* <button
+                                            onClick={prevWorkout}
+                                            className="bg-blue-700 active:bg-blue-900 text-white rounded-full font-bold p-3 mx-2 text-5xl transition hover:bg-blue-500 hover:-translate-y-1 duration-500"
+                                        >
+                                            <FaArrowAltCircleLeft/>
+                                        </button> */}
+                                        
+                                        <a
+                                            onClick={prevWorkout}
+                                            className="items-center flex text-blue-700  active:text-blue-900 rounded-full font-bold mx-2 text-5xl transition hover:scale-105 hover:-translate-y-1 duration-500"
+                                        >
+                                            <FaArrowAltCircleLeft/>
+                                        </a>
+
+                                        <div className="flex justify-center font-bold my-5 text-lg">
+                                            <p className='border-4 border-blue-700 text-white bg-blue-700 p-3 px-6 rounded-full select-none' >
+                                                Treino {currentWorkoutIndex + 1} de {workouts.length}
+                                            </p>
+                                        </div>
+
+                                        {/* <button
+                                            onClick={nextWorkout}
+                                            className="bg-blue-700 active:bg-blue-900 text-white rounded-full font-bold p-3 mx-2 text-5xl transition hover:bg-blue-500 hover:-translate-y-1 duration-500"
+                                        >
+                                            <FaArrowAltCircleRight/>
+                                        </button> */}
+                                        
+                                        <a
+                                            onClick={nextWorkout}
+                                            className="items-center flex text-blue-700  active:text-blue-900 rounded-full font-bold mx-2 text-5xl transition hover:scale-105 hover:-translate-y-1 duration-500"
+                                        >
+                                            <FaArrowAltCircleRight/>
+                                        </a>
                                     </div>
-                                    <div className="flex justify-center text-lg rounded-xl p-5 my-5 mx-3 border-4 text-blue-700 border-blue-700 font-bold md:m-5 whitespace-pre-line overflow-y-auto">
+                                    <div className="flex justify-center text-lg rounded-xl p-5 my-5 mx-3 border-4 text-white border-blue-700 bg-blue-700 font-bold md:m-5 whitespace-pre-line overflow-y-auto">
                                         <div className="fixed-width md:fixed-lg-width">
                                             {renderSeries()}
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-
-                            <div className="flex justify-center mt-4">
-                                <button
-                                    onClick={prevWorkout}
-                                    className="bg-blue-700 active:bg-blue-900 text-white rounded-full font-bold p-3 mx-2 transition hover:bg-blue-500 hover:-translate-y-1 duration-500"
-                                >
-                                    Treino Anterior
-                                </button>
-                                <button
-                                    onClick={nextWorkout}
-                                    className="bg-blue-700 active:bg-blue-900 text-white rounded-full font-bold p-3 mx-2 transition hover:bg-blue-500 hover:-translate-y-1 duration-500"
-                                >
-                                    Próximo Treino
-                                </button>
                             </div>
                         </section>
                     ) : (
