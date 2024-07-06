1e9c90f7bfa54f86ac69ebf2c1724c93b1f7c9fa
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-07-06 16:54:07 -0300

    ccors no credential

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ const allowedOrigins = ['http://localhost:5173', 'http://localhost:5000', 'https
 const corsOptions = {
     origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions))
