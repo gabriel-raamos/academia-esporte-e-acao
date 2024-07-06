96b27740323c8ee4c9cfea375e59012f637f0249
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-07-06 16:09:10 -0300

    changed cors origin to *

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 // const allowedOrigins = ['http://localhost:5173', 'http://localhost:5000', 'https://pi-academia.vercel.app'];
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
