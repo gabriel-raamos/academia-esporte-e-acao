7e304657aa0d7579543f190e72d55b3824375ba5
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-07-07 01:30:56 -0300

    corrected endpoints at pagamentolista

--- src/pages/PagamentoLista/PagamentoLista.jsx
+++ src/pages/PagamentoLista/PagamentoLista.jsx
@@ -28,7 +28,7 @@ export default function PagamentoLista() {
 
     const fetchPagamentos = async () => {
         try {
-            const response = await axios.get(`https://pi-academia.vercel.app/api/pagamento/mostrarpagamentosreverso`, { withCredentials: true });
+            const response = await axios.get(`https://academia-esporte-e-acao.vercel.app/api/pagamento/mostrarpagamentosreverso`, { withCredentials: true });
             // console.log("Dados da API de pagamentos:", response.data); // Adicionando log para ver os dados
 
             const pagamentosData = response.data;
@@ -37,7 +37,7 @@ export default function PagamentoLista() {
             if (Array.isArray(pagamentosData)) {
                 const pagamentosConvertidos = await Promise.all(
                     pagamentosData.map(async (pagamento) => {
-                        const clienteResponse = await axios.get(`https://pi-academia.vercel.app/api/cliente/findbyid/${pagamento.clienteID}`);
+                        const clienteResponse = await axios.get(`https://academia-esporte-e-acao.vercel.app/api/cliente/findbyid/${pagamento.clienteID}`);
                         const cliente = clienteResponse.data;
 
                         return {
