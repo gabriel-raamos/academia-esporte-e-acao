f3d437bc7cb1cf923fd761ad6ab3726cb02801df
gabriel-raamos <gabriel.ramos.galvao@hotmail.com>
2024-07-11 16:24:18 -0300

    changed the footer, now the year will update automatically

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -1,8 +1,11 @@
 export default function Rodape() {
+
+    const date = new Date()
+
     return (
         <footer className="flex flex-col" >
             <div className="bg-slate-800 text-center bottom-0 p-4 text-white footer">
-                    © 2024 Copyright
+                    © {date.getFullYear()} Academia Esporte e Ação.
             </div>
         </footer>
     );
